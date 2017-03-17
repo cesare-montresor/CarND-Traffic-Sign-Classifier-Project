edf137dff13ba1d015d178ff2d424c8a2a6e34f5
Cesare Montresor <cesare.montresor@gmail.com>
2017-03-17 14:43:25 +0100

    cleanup

--- downloadDataset.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-mkdir -p dataset/
-cd dataset/
-wget https://d17h27t6h515a5.cloudfront.net/topher/2017/February/5898cd6f_traffic-signs-data/traffic-signs-data.zip
-unzip traffic-signs-data.zip -d traffic-signs-data
